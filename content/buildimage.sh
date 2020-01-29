211458f76a451c6cc3ab42ae9636385305689f01
catcherwong <catcher_hwq@outlook.com>
2020-01-29 16:40:05 +0800

    :whale: build docker

--- content/buildimage.sh
+++ content/buildimage.sh
@@ -1,3 +1,18 @@
-tag=`date +%Y%m%d%H%M%S`
+env=""
 
-docker build -t apitpl:${tag} .
+if [ -n "$1" ]; then
+    env=$1
+else
+    echo "Please enter the env param!!"
+    exit 1
+fi
+
+d=`date +%Y%m%d%H%M%S`
+tag=${d}
+
+echo apitpl_${env}:${tag}
+docker build -t apitpl_${env}:${tag} -f docker/Dockerfile.${env} .
+
+# docker stop apitpl_${env} 
+# docker rm apitpl_${env} 
+# docker run -d -p 9000:80 --name apitpl_${env} 
